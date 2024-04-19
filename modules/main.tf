77d2bba1c3aa071216af379f588519c5ec7d0d69
chaitu <chaitus.ch09@gmail.com>
2024-04-19 05:15:08 -0700

    adding promethus.yml (automation code)

--- modules/main.tf
+++ modules/main.tf
@@ -2,6 +2,8 @@ resource "aws_instance" "instance" {
   ami=data.aws_ami.ami.image_id
   instance_type = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.selected.id]
+  #prometheus
+  iam_instance_profile   = aws_iam_instance_profile.instance_profile.name
 
   tags = {
     Name    = var.tool_name
@@ -13,10 +15,52 @@ resource "aws_instance" "instance" {
 
 
 resource "aws_route53_record" "record" {
-  name="${var.tool_name}}"
+  name="${var.tool_name}-role"
   type="A"
   zone_id=var.zone_id
   records = [aws_instance.instance.public_ip]
   ttl=30
 
+}
+# prometheus -node extractor
+resource "aws_iam_role" "role" {
+  name = "${var.tool_name}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+#  inline_policy {
+#    name = "${var.tool_name}-inline-policy"
+#
+#    policy = jsonencode({
+#      Version = "2012-10-17"
+#      Statement = [
+#        {
+#          Action   = concat(var.dummy_policy, var.policy_resource_list)
+#          Effect   = "Allow"
+#          Resource = "*"
+#        },
+#      ]
+#    })
+#  }
+
+  tags = {
+    Name = "${var.tool_name}-role"
+  }
+}
+
+resource "aws_iam_instance_profile" "instance_profile" {
+  name="${var.tool_name}-role"
+  role=aws_iam_role.role.name
 }
