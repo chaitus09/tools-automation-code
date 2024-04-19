31fb29c23c6481db6b88535954da327481195172
chaitu <chaitus.ch09@gmail.com>
2024-04-19 11:30:06 -0700

    adding policy to the ec2

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "tools" {
   default = {
-    prometheus={
-      instance_type= "t3.small"
-      policy_resources_list=["ec2:DescribeInstances"]
+    prometheus  = {
+      instance_type         = "t3.small"
+      policy_resources_list =  ["ec2:DescribeInstances"]
     }
   }
 }
 variable "zone_id" {
-  default="Z1029901SH2BJPKJS7Q3"
+  default= "Z1029901SH2BJPKJS7Q3"
 }
