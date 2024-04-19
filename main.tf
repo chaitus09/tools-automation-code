408d8084a12991bd0f047a4879e9e96b9a40ea8a
chaitu <chaitus.ch09@gmail.com>
2024-04-19 10:49:37 -0700

    adding instance profile

--- main.tf
+++ main.tf
@@ -4,6 +4,6 @@ for_each=var.tools
 
   tool_name=each.key
   instance_type=each.value["instance_type"]
-  //policy_resource_list=each.value["policy_resource_list"]
+#  policy_resource_list=each.value["policy_resource_list"]
   zone_id = var.zone_id
 }
